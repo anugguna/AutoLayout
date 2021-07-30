e5d02e51d633e9aec4d41a1386b90f25ddc9d11d
anugguna <87663740+anugguna@users.noreply.github.com>
2021-07-30 13:44:11 +0300

    Button background colours reset after refresh

--- AutoLayout/Controller/ViewController.swift
+++ AutoLayout/Controller/ViewController.swift
@@ -31,6 +31,9 @@ class ViewController: UIViewController {
         self.buttonDOutlet.isHidden = false
         self.buttonEOutlet.isHidden = false
         self.buttonAOutlet.setImage(UIImage(systemName: "no.image"), for: .normal)
+        self.buttonCOutlet.backgroundColor = #colorLiteral(red: 0.2938342207, green: 0.8516694493, blue: 0.9358661482, alpha: 1)
+        self.buttonDOutlet.backgroundColor = #colorLiteral(red: 0.2708843913, green: 0.7571885466, blue: 0.8270784575, alpha: 1)
+        self.buttonEOutlet.backgroundColor = #colorLiteral(red: 0.172448346, green: 0.6291153209, blue: 0.5037078612, alpha: 1)
         
         
     }
